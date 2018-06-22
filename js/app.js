c5e802ff88edf77a680091597b22fec4023c7dd6
KatyPreston <prestonkj@hotmail.com>
2018-06-22 12:00:20 +0100

    working but not saving each entry

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("JavaScript Loaded");
+
+
+  const form = document.querySelector("#new-item-form");
+    console.log(form);
+    const handleFormSubmit = function(event){
+      event.preventDefault();
+
+      console.log("form logged")
+      const form = event.target;
+
+      const country = form.country.value;
+      const reason = form.reason.value;
+      const urgency = form.urgency.value;
+
+      const result = document.querySelector("#travel-bucket-list");
+      result.textContent = `${country}, ${reason}, ${urgency}`;
+
+      form.reset();
+    }
+
+
+  form.addEventListener("submit", handleFormSubmit);
+
 });
